90577dc597eb0efdb2c511f2b550ca956708fc59
caio souza <caiocbahia@hotmail.com>
2022-06-14 00:10:50 -0300

    Test get request from api working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular_mat_exercicio_bp';
+  url = 'https://swapi.dev/api/planets/1/'
 
   constructor(private http: HttpClient){
-    
+    this.http.get(this.url).toPromise().then(data => {
+      console.log(data)
+    })
   }
 }
+
+//A request get é "observable", mas ñ faz sentido observar pois a request
+//só é feita uma vez
