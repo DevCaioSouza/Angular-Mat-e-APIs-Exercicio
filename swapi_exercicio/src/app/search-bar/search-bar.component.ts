3f97a5cfa2eba2f227e8d7eac08c03cc3144869e
caio souza <caiocbahia@hotmail.com>
2022-06-16 16:58:57 -0300

    anotações - event binding Aula 172

--- swapi_exercicio/src/app/search-bar/search-bar.component.ts
+++ swapi_exercicio/src/app/search-bar/search-bar.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./search-bar.component.css']
 })
 export class SearchBarComponent {
+  // <string>: chamado de Generic Annotation
   @Output() submitted = new EventEmitter<string>()
+  // o submitted é o nome (um id) do evento que irá depois ser
+  // vinculado via event binding no comp. pai. É usado para
+  // passar uma informação que será ativada pelo comp. pai
 
   term = '';
 
@@ -17,3 +21,10 @@ export class SearchBarComponent {
     this.submitted.emit(this.term)
   }
 }
+
+/*
+Anotações da aula 172 - Passo a passo:
+1° Fazer os imports EventEmitter e Output no componente filho
+2° Chamar a diretiva @Output no filho
+
+*/
