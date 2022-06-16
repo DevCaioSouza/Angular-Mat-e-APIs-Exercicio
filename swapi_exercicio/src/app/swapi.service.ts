209ef0f9326b18a26b9b60da6dd4d091500f4bb8
caio souza <caiocbahia@hotmail.com>
2022-06-16 19:15:03 -0300

    Notes on Services (on swapi.service.ts)

--- swapi_exercicio/src/app/swapi.service.ts
+++ swapi_exercicio/src/app/swapi.service.ts
@@ -7,3 +7,12 @@ export class SwapiService {
 
   constructor() { }
 }
+
+/*
+---------- SERVICES ----------
+- Used to fetch / store / update any kind of data in our app
+- Almost Always where we are going to put network requests
+- Data flows from a service to a component
+- Implemented as classes
+- Angular will automatically create a single instance of each service for us
+*/
