1382ad035f969394057fdf4608ac401b497c0a96
caio souza <caiocbahia@hotmail.com>
2022-06-16 15:49:54 -0300

    Event binding between a child and a parent comp

--- swapi_exercicio/src/app/search-bar/search-bar.component.ts
+++ swapi_exercicio/src/app/search-bar/search-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
@@ -6,12 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-bar.component.css']
 })
 export class SearchBarComponent {
+  @Output() submitted = new EventEmitter<string>()
+
   term = '';
 
   constructor() { }
 
   onFormSubmit(event: any){
     event.preventDefault()
-    console.log(this.term)
+    this.submitted.emit(this.term)
   }
 }
