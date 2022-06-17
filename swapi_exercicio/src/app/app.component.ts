e71d268580d8dbdfc1b02f63ec3d43b20435ca14
caio souza <caiocbahia@hotmail.com>
2022-06-17 11:45:36 -0300

    Input binding works - shows length of API array

--- swapi_exercicio/src/app/app.component.ts
+++ swapi_exercicio/src/app/app.component.ts
@@ -7,13 +7,14 @@ import { SwapiService } from './swapi.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(private swapi: SwapiService){
+  pages = [];
 
-  }
+  constructor(private swapi: SwapiService){}
 
   onTerm(term: string){
-    this.swapi.search(term).subscribe((response) => {
-      console.log(response)
+    this.swapi.search(term).subscribe((response: any) => {
+      console.log(response.results.length)
+      this.pages = response.results
     })
   }
 }
