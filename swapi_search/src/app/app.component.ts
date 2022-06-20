e4d3eab56fcad082e386a93fab5283a5c645a8ec
caio souza <caiocbahia@hotmail.com>
2022-06-20 11:25:02 -0300

    ts ready

--- swapi_search/src/app/app.component.ts
+++ swapi_search/src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+
+interface PeopleData {
+  name: string;
+  birthYear: string;
+  height: number;
+  weight: number;
+  eyeColor: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'swapi_search';
+export class AppComponent implements OnInit {
+  searchResult$!: Observable<PeopleData>;
+
+  search = new FormControl();
+
+  searchFormGroup = new FormGroup({
+    search: this.search,
+  });
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.searchResult$ = this.search.valueChanges.pipe(
+      switchMap(term =>
+        this.http.get<any>(`https://swapi.dev/api/people/?search=${term}`)
+      ),
+      map(response =>
+        response.count > 0 ? response.results[0] : {name: "No Results"}
+      ),
+      map(
+       response =>
+        ({
+          name: response.name,
+          birthYear: response.birth_year,
+          height: Number(response.height),
+          weight: Number(response.mass),
+          eyeColor: response.eye_color
+        }as PeopleData)
+      )
+    )
+  }
 }
